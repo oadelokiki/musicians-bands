d8a63fa9b218db8ca94adabd4e6d759bd7640f1f
ranktel <ranktel@worktop.myguest.virtualbox.org>
2023-03-11 21:28:19 -0500

    Bands part 3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+const {Song} = require('./Song')
 
-Band.Musician= Band.hasOne(Musician)
+//Band.Musician = Band.hasOne(Musician)
+Band.hasMany(Musician)
+Musician.belongsTo(Band)
+
+Band.hasMany(Song)
+Song.hasMany(Band)
 
 module.exports = {
     Band,
     Musician,
-	
+	Song	
 };
 
 
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const {sequelize} = require('./db');
-const {Band, Musician} = require('./index')
+const {Band, Musician, Song} = require('./index')
 
 describe('Band and Musician Models', () => {
     /**
@@ -23,30 +23,97 @@ describe('Band and Musician Models', () => {
     })
 
     test('can create a Musician', async () => {
-        // TODO - test creating a musician
+        
+	//TODO --> test creating a musician
+	
 	let testMusician = await Musician.create({
 		name: "ben",
 		instrument: "oboe"
 	})
+	
         expect(testMusician.name).toBe('ben');
 	expect(testMusician.instrument).toBe('oboe')
+    	
     })
 
-	test('association', async () => {
+    test('one to many association', async () => {
 	let testBand = await Band.create({
 		name: "the jets",
 		genre: "rock",
-		Musician:  {
+		
+		Musicians: [ {
 			name: "benny",
 			instrument: "guitar"
-		}
+		}]
 
 	}, {
 		include: [
-			Band.Musician	
+			//Band.Musician	
+			Musician
 		]	
 	})
-		console.log(testBand.Musician)
-		expect(testBand.Musician.name).toBe("benny")
+		console.log(testBand.Musicians)
+	expect(testBand.Musicians[0].name).toBe("benny")
 	})
+
+	test('many to many association', async () => {
+        
+	const testBand = await Band.create({
+                name: "the jets",
+                genre: "rock",
+                Musicians: [{
+                        name: "benny",
+                        instrument: "guitar"
+                	
+		}],
+		Songs: [{
+			title: "rocket man",
+			year: 1901,
+
+		}, {
+			title: "these are alright",
+			year: 2012,
+
+		}]
+        }, {
+                include: [
+                        //Band.Musician
+			Musician, Song,
+                
+		]
+        })
+                console.log(testBand.Songs)
+
+	// TODO now query for bands with a particular song, and songs with a particular band
+
+	let partSongResults = await testBand.getSongs();
+
+	console.log(partSongResults.Songs);
+
+        expect(partSongResults.length).toBe(2)
+
+	let testSong = await Song.create({
+		title: "hello there knowledge",
+		year: 2021,
+		Bands: [{
+			name: "the thrills",
+			genre: "indie",
+			Musicians: []
+		}, {
+			name: "the beatles",
+			genre: "indie",
+			Musicians: []
+		}]
+		
+	}, {
+		include: [
+			Band, 
+		]
+	})
+	
+	let partBandResults = await testSong.getBands();
+	console.log("your results" + partBandResults);
+	expect(partBandResults.length).toBe(2)
+        })
+
 })
